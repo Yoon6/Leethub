dd7e583eacb885413a092d5366468879bef6cf18
Yoon <49135657+Yoon6@users.noreply.github.com>
2025-06-26 23:32:02 +0900

    Time: 4 ms (31.91%), Space: 9 MB (7.53%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -1,33 +1,34 @@
 #include <string>
+#include <sstream>
 #include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
-        string result;
+        stringstream result;
         int minLength = min(word1.length(), word2.length());
 
         for (int i = 0; i < minLength; i++)
         {
-            result += word1[i];
-            result += word2[i];
+            result << word1[i];
+            result << word2[i];
         }
 
         if (word1.length() > minLength)
         {
             for (int i = minLength; i < word1.length(); i++)
             {
-                result += word1[i];
+                result << word1[i];
             }
         }
         else if (word2.length() > minLength)
         {
             for (int i = minLength; i < word2.length(); i++)
             {
-                result += word2[i];
+                result << word2[i];
             }
         }
-        return result;
+        return result.str();
     }
 };
