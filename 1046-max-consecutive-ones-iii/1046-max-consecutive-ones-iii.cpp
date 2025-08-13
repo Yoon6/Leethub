ae4e44b49ffce86e648833e58407f88019adb1f0
Yoon <49135657+Yoon6@users.noreply.github.com>
2025-08-14 02:59:32 +0900

    Time: 1149 ms (5.28%), Space: 69.5 MB (19.18%) - LeetHub

---
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+
+        int maxCount = 0;
+        int flipCount = 0;
+        int fir = 0;
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            fir = i;
+            if (nums[fir] == 0)
+            {
+                if (flipCount < k)
+                {
+                    flipCount++;
+                    maxCount++;
+                    break;
+                }
+            }
+            else
+            {
+                maxCount++;
+                break;
+            }
+        }
+
+        int count = maxCount;
+        for (int i = fir + 1; i < nums.size(); ++i)
+        {
+            if (nums[i] == 0)
+            {
+                if (k == 0)
+                {
+                    count = 0;
+                    fir = i;
+                }
+                else if (flipCount < k)
+                {
+                    flipCount++;
+                    count++;
+                }
+                else
+                {
+                    if (nums[fir] == 0)
+                    {
+                        fir++;
+                    }
+                    else
+                    {
+                        while (fir < i)
+                        {
+                            count--;
+                            if (nums[++fir] == 0)
+                            {
+                                fir++;
+                                break;
+                            }
+                        }
+                        
+                    }
+                }
+            }
+            else
+            {
+                count++;
+            }
+            cout << count << endl;
+
+            maxCount = max(count, maxCount);
+        }
+        
+        return maxCount;
+    }
+};
